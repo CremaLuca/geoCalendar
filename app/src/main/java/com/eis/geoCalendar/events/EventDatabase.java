1dc04c855579ec82c4dd9466f6d90c6d27f59f2a
Luca Crema <lc.crema@hotmail.com>
2019-12-23 11:06:56 +0100

    Added collection methods to EventDatabase interface

--- app/src/main/java/com/eis/geoCalendar/events/EventDatabase.java
+++ app/src/main/java/com/eis/geoCalendar/events/EventDatabase.java
@@ -1,8 +1,9 @@
 package com.eis.geoCalendar.events;
 
-import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Handles memorization of events in phone memory.
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
  * @since 21/21/2019
  */
 public interface EventDatabase<E extends Event> {
+
     /**
      * Saves an event in memory.
      *
@@ -21,11 +23,26 @@ public interface EventDatabase<E extends Event> {
     void saveEvent(@NonNull final E event);
 
     /**
+     * Saves a list of events in memory.
+     *
+     * @param events The list of events.
+     */
+    void saveEvents(@NonNull final Collection<E> events);
+
+    /**
      * Removes an event from memory.
      *
      * @param event The event.
      */
     void removeEvent(@NonNull final E event);
+
+
+    /**
+     * Removes a list of events from memory.
+     *
+     * @param events The list of events.
+     */
+    void removeEvents(@NonNull final Collection<E> events);
 
     /**
      * Retrieves all saved events from memory.

bf5838d60ff55c14c2bba00edbae67799151cb7b
francescobau <56580849+francescobau@users.noreply.github.com>
2019-12-24 16:11:45 +0100

    Added PermissionDialogManager to prompt a Dialog to notify a user about a permission, added GenericEvent, edited GenericTimedEvent.
    DISCLAIMER: Still under development.
    
    KNOWN ISSUES:
    - Useless comments needs to be cleaned up.
    - Now it manages one single permission.
    - Might be invasive
    - ...

--- app/src/main/java/com/eis/geoCalendar/app/GenericEvent.java
+++ app/src/main/java/com/eis/geoCalendar/app/GenericEvent.java
@@ -1,5 +1,6 @@
 package com.eis.geoCalendar.app;
 
+import com.eis.geoCalendar.events.Event;
 import com.eis.geoCalendar.gps.GPSPosition;
 import com.eis.geoCalendar.timedEvents.DateTime;
 import com.eis.geoCalendar.timedEvents.TimedEvent;
@@ -8,32 +9,25 @@ import com.eis.geoCalendar.timedEvents.TimedEvent;
  * Manager of timed events' implementation.
  *
  * @param <T> Type of event.
- * @author Francesco Bau' helped by Luca Crema.
+ * @author Francesco Bau'
  * @version 1.0
  * @see com.eis.geoCalendar.events.EventManager
- * @since 23/12/2019
+ * @since 24/12/2019
  */
-public class GenericTimedEvent<T> extends GenericEvent<T> implements TimedEvent<T> {
+public class GenericEvent<T> implements Event<T> {
 
-    protected DateTime time;
+    protected GPSPosition position;
+    protected T content;
+
     /**
      * Main constructor, which acquires all details of the just created event.
      *
      * @param position The position of the event.
      * @param content  The content of the event.
-     * @param time     The time/deadline of the event.
      */
-    public GenericTimedEvent(GPSPosition position, T content, DateTime time) {
-        super(position,content);
-        this.time = time;
-    }
-
-    /**
-     * @return the time/deadline of the event.
-     */
-    @Override
-    public DateTime getTime() {
-        return time;
+    public GenericEvent(GPSPosition position, T content) {
+        this.position = position;
+        this.content = content;
     }
 
     /**

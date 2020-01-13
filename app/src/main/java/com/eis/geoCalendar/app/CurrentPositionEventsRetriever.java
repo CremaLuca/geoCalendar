836106f2076fcd6bcb556fa800393ea0273b7cbe
Alessandra Tonin <ale.tonin.98@gmail.com>
2020-01-14 00:33:40 +0100

    Created class CurrentPositionEventsRetriever

---
+++ app/src/main/java/com/eis/geoCalendar/app/CurrentPositionEventsRetriever.java
@@ -0,0 +1,39 @@
+package com.eis.geoCalendar.app;
+
+import com.eis.geoCalendar.app.network.GenericNetworkedEventManager;
+import com.eis.geoCalendar.network.GetEventListener;
+import com.eis.geoCalendar.network.NetworkEvent;
+
+/**
+ * This class handles the retrieving of the events near to the user's position
+ *
+ * @author Tonin Alessandra, Luca Crema
+ */
+public class CurrentPositionEventsRetriever<E extends NetworkEvent> {
+    /**
+     * Constructor
+     * //TODO
+     */
+    CurrentPositionEventsRetriever(GenericNetworkedEventManager eventManager, GetEventListener<E> listener, float precision) {
+    }
+
+    ;
+
+    /**
+     * //TODO
+     */
+    void startEventRetrieval() {
+    }
+
+    ;
+
+    /**
+     * //TODO
+     */
+    void stopEventRetrieval() {
+    }
+
+    ;
+
+
+}

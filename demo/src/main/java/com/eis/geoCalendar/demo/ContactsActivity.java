00bbe602ce801b99c5b0b22f6297bd7c404d9227
Alessandra Tonin <ale.tonin.98@gmail.com>
2020-02-27 17:29:10 +0100

    Added friends' list saving on sharedPreferences

--- demo/src/main/java/com/eis/geoCalendar/demo/ContactsActivity.java
+++ demo/src/main/java/com/eis/geoCalendar/demo/ContactsActivity.java
@@ -2,19 +2,25 @@ package com.eis.geoCalendar.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * This class handles a friend's list, picking up contacts from the device's address book.
+ * This class handles a friend's list, picking up contacts from the device's address book and saving them into app's sharedPreferences file.
  *
  * @author Tonin Alessandra
  */
@@ -36,6 +42,9 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         scrollLinLayout = findViewById(R.id.scrollLinLayout);
+
+        //this method takes friends' list saved in preferences and visualizes them in this activity
+        loadContacts();
     }
 
     /**
@@ -80,15 +89,53 @@ public class ContactsActivity extends AppCompatActivity {
 
                     String contactInfo = name + SPACE + number;
 
-                    //add a TextView to the LinearLayout
-                    TextView contact = new TextView(this);
-                    contact.setText(contactInfo);
-                    scrollLinLayout.addView(contact);
+                    //save new friend in preferences, only if it is not yet saved, and display it n the current activity
+                    if (ContactsActivity.this.getPreferences(Context.MODE_PRIVATE).getString(number, null) == null) {
+                        savePreferences(number, name);
+                        //add a TextView to the LinearLayout
+                        TextView contact = new TextView(this);
+                        contact.setText(contactInfo);
+                        scrollLinLayout.addView(contact);
+                    }
+
                 } else {
                     Toast.makeText(getApplicationContext(), NO_NUMBER, Toast.LENGTH_LONG).show();
                 }
                 cursor.close();
             }
         }
+    }
+
+    /**
+     * This method takes friends' list saved in preferences and displays them
+     */
+    private void loadContacts() {
+        //1. take contacts from preferences
+        //2. display them (add textViews to scrollLinLayout
+        SharedPreferences sharedPref = ContactsActivity.this.getPreferences(Context.MODE_PRIVATE);
+        Map<String, ?> storedFriends = sharedPref.getAll();
+        HashMap<String, String> storedFriendsHashMap = (HashMap<String, String>) storedFriends;
+        // Iterate all key/value pairs
+        for (Map.Entry<String, String> friend : storedFriendsHashMap.entrySet()) {
+            String contactInfo = friend.getValue() + SPACE + friend.getKey();
+            TextView contact = new TextView(this);
+            contact.setText(contactInfo);
+            scrollLinLayout.addView(contact);
+        }
+
+    }
+
+    /**
+     * This method saves friends' list on sharedPreferences file
+     *
+     * @param key   The number of the new friend
+     * @param value The name of the new friend
+     */
+    private void savePreferences(String key, String value) {
+        SharedPreferences sharedPref = ContactsActivity.this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        //save key-value pairs: key is the number, value is the name
+        editor.putString(key, value);
+        editor.apply();
     }
 }

dc03c5b79c228e21b67929f4e01088d093b2d908
Alessandra Tonin <ale.tonin.98@gmail.com>
2020-03-05 18:34:19 +0100

    Fixed some bugs on permissions' request and layout improvements

--- demo/src/main/java/com/eis/geoCalendar/demo/MapsActivity.java
+++ demo/src/main/java/com/eis/geoCalendar/demo/MapsActivity.java
@@ -66,7 +66,6 @@ public class MapsActivity extends FragmentActivity {
         EventBottomSheetDialogFragment eventBottomSheetDialogFragment = new EventBottomSheetDialogFragment();
 
 
-
     }
 
     /***
